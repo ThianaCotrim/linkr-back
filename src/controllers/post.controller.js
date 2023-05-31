38cc2d474bf17537c9195832ad62988aa825497f
yurilopesm <yurilopesm@hotmail.com>
2023-05-31 18:15:03 -0300

    fix: update url-metadata lib version

--- src/controllers/post.controller.js
+++ src/controllers/post.controller.js
@@ -5,11 +5,16 @@ export async function createPost(req, res) {
     const {link, description} = req.body
     const {userId} = res.locals.session
     try{
+        const metadata = await getMetadata(link)
+        console.log(metadata)
         await newPost(userId, link, description)
         res.sendStatus(201)
     }catch (err) {
         res.status(500).send(err.message)
     }
+}
+async function getMetadata(link) {
+    return urlMetadata(link)
 }
 
 export async function getAllPosts(req, res) {
