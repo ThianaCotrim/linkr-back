c8c1ba1bd51051ac22bd3fa598171740af13e7a9
Kaiena <kaienafmartins@gmail.com>
2023-05-31 18:52:17 -0300

    create edit and delete post route

--- src/controllers/post.controller.js
+++ src/controllers/post.controller.js
@@ -1,35 +1,66 @@
-import { allHashtags } from "../repositories/posts.repository.js";
+import {
+  allHashtags,
+  getSpecificHashtagDB,
+  editPostDB,
+  deletePostDB,
+} from "../repositories/posts.repository.js";
 
 export async function createPost(req, res) {
-	try {
-		res.sendStatus(200);
-	} catch (err) {
-		res.status(500).send(err.message);
-	}
+  try {
+    res.sendStatus(200);
+  } catch (err) {
+    res.status(500).send(err.message);
+  }
 }
+
 export async function getAllPosts(req, res) {
-	try {
-		res.sendStatus(200);
-	} catch (err) {
-		res.status(500).send(err.message);
-	}
+  try {
+    res.sendStatus(200);
+  } catch (err) {
+    res.status(500).send(err.message);
+  }
 }
+
 export async function getHashtags(req, res) {
-	try {
-		const hashtags = await allHashtags();
-		res.send(hashtags.rows);
-	} catch (err) {
-		res.status(500).send(err.message);
-	}
+  try {
+    const hashtags = await allHashtags();
+    res.send(hashtags.rows);
+  } catch (err) {
+    res.status(500).send(err.message);
+  }
 }
+
 export async function getSpecificHashtag(req, res) {
-	const { hashtag } = req.params;
-	try {
-		const hashtags = await getSpecificHashtagDB(hashtag);
-		if (hashtags.rows.length === 0)
-			return res.status(404).send({ message: "Nenhuma hashtag encontrada" });
-		res.send(hashtags.rows);
-	} catch (err) {
-		res.status(500).send(err.message);
-	}
+  const { hashtag } = req.params;
+  try {
+    const hashtags = await getSpecificHashtagDB(hashtag);
+    if (hashtags.rows.length === 0)
+      return res.status(404).send({ message: "Nenhuma hashtag encontrada" });
+    res.send(hashtags.rows);
+  } catch (err) {
+    res.status(500).send(err.message);
+  }
+}
+
+export async function editPost(req, res) {
+  const { id } = req.params;
+  const { link, description } = req.body;
+
+  try {
+    await editPostDB(id, link, description);
+    res.sendStatus(200);
+  } catch (error) {
+    res.status(500).send(error.message);
+  }
+}
+
+export async function deletePost(req, res) {
+  const { id } = req.params;
+
+  try {
+    await deletePostDB(id);
+    res.sendStatus(200);
+  } catch (error) {
+    res.status(500).send(error.message);
+  }
 }
--- src/routes/post.routes.js
+++ src/routes/post.routes.js
@@ -1,11 +1,16 @@
 import { Router } from "express";
 import { tokenValidation } from "../middlewares/tokenValidation.js";
 import {
-	createPost,
-	getAllPosts,
-	getHashtags,
-	getSpecificHashtag,
+  createPost,
+  getAllPosts,
+  getHashtags,
+  getSpecificHashtag,
+  editPost,
+  deletePost,
 } from "../controllers/post.controller.js";
+import { postSchema } from "../schemas/post.schema.js";
+import { validateSchema } from "../middlewares/validateSchema.js";
+import { EditPostValidation } from "../middlewares/post.middleware.js";
 
 const postRouter = Router();
 
@@ -13,5 +18,13 @@ postRouter.post("/posts", tokenValidation, createPost);
 postRouter.get("/posts", tokenValidation, getAllPosts);
 postRouter.get("/hashtag", tokenValidation, getHashtags);
 postRouter.get("hashtag/:hashtag", tokenValidation, getSpecificHashtag);
+postRouter.put(
+  "/posts/:id",
+  validateSchema(postSchema),
+  tokenValidation,
+  EditPostValidation,
+  editPost
+);
+postRouter.delete("/posts/:id", tokenValidation, deletePost);
 
 export default postRouter;
