aa89803416d3f751ae0531ce2c0ffaf1f0514fbb
Kaiena <kaienafmartins@gmail.com>
2023-06-02 18:56:19 -0300

    remove link from edit route

--- src/controllers/post.controller.js
+++ src/controllers/post.controller.js
@@ -1,6 +1,5 @@
-
-import urlMetadata from "url-metadata"
-import { getPosts, newPost } from "../repositories/posts.repository.js"
+import urlMetadata from "url-metadata";
+import { getPosts, newPost } from "../repositories/posts.repository.js";
 
 import {
   allHashtags,
@@ -11,31 +10,30 @@ import {
 import { theMetadata } from "../repositories/metadata.repository.js";
 
 export async function createPost(req, res) {
-    const {link, description} = req.body
-    const {userId} = res.locals.session
-    try{
-        const {rows: response} = await newPost(userId, link, description)
-        const postId = response[0].id
+  const { link, description } = req.body;
+  const { userId } = res.locals.session;
+  try {
+    const { rows: response } = await newPost(userId, link, description);
+    const postId = response[0].id;
 
-        const metadata = urlMetadata(link).then((response) => {
-            const { title, image, description} = response
-            theMetadata(title, description, image, postId)
-        })
-        res.sendStatus(201)
-    }catch (err) {
-        res.status(500).send(err.message)
-    }
-
+    const metadata = urlMetadata(link).then((response) => {
+      const { title, image, description } = response;
+      theMetadata(title, description, image, postId);
+    });
+    res.sendStatus(201);
+  } catch (err) {
+    res.status(500).send(err.message);
+  }
+}
 
 async function getMetadata(link) {
   return urlMetadata(link);
-
 }
 
 export async function getAllPosts(req, res) {
   try {
-    const {rows: posts} = await getPosts()
-    res.status(200).send(posts)
+    const { rows: posts } = await getPosts();
+    res.status(200).send(posts);
   } catch (err) {
     res.status(500).send(err.message);
   }
@@ -64,10 +62,10 @@ export async function getSpecificHashtag(req, res) {
 
 export async function editPost(req, res) {
   const { id } = req.params;
-  const { link, description } = req.body;
+  const { description } = req.body;
 
   try {
-    await editPostDB(id, link, description);
+    await editPostDB(id, description);
     res.sendStatus(200);
   } catch (error) {
     res.status(500).send(error.message);
